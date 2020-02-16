281c57a89e9073f1a040f4b4cfa4fe82b98d8df3
Roterior <Roterior@gmail.com>
2020-02-17 01:52:25 +0300

    add methods to get html_code and cache for browser

--- src/connect.py
+++ src/connect.py
@@ -1,34 +1,90 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import sys
+import traceback
+from os.path import abspath
+from time import sleep
 
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from time import sleep
+from selenium.common.exceptions import WebDriverException
 
 
 def connection():
-    print('Starting...')
     # link_bet_cs_go = 'https://betscsgo.me'
-    # link_gg_bet = 'https://gg.bet/ru/betting'
-    # driver = webdriver.Chrome('../resource/lib/chrome_driver.exe')
-    # driver.get(link_bet_cs_go)
-    # sleep(15)
-    # html_code = driver.page_source
-    # driver.close()
+    # data = get_html(link_bet_cs_go)
 
-    test_data_file = open('../resource/data/html_bet_cs_go.txt', 'r', encoding='utf-8')
-    data = test_data_file.readlines()
-    test_data_file.close()
-    # for i, line in enumerate(data):
-    #     print(line.strip())
-    #     if '<div class="sys-timerange-header timerange__header">Текущие события</div>' in line:
-    #         print(i)
+    path = '../resource/data/html_bet_cs_go.txt'
+    data = get_test_html(path)
 
-    print(data[2048].strip())
-    print(data[2055].strip())
-
-    # soup = BeautifulSoup(html_code, 'html.parser')
-    # file = open('../output', 'w', encoding="utf-8")
-    # file.write(str(html_code))
+    # file = open('../resource/data/output.txt', 'w', encoding="utf-8")
+    # file.write(data)
     # file.close()
-    print("Finishing...")
+
+    """
+    --- PROCESSING DATA ---
+    """
+
+    parsed_data = parse(data)
+    print(parsed_data)
+
+    # num = 0
+    # for i, line1 in enumerate(data):
+    #     if 'timerange timerange_now sys-timerange' in line1:
+    #         num = i
+    # row = data[num+2].strip()
+    # temp = []
+    # cur = ''
+    # for letter in row:
+    #     if letter == '>':
+    #         temp.append(cur.strip() + letter)
+    #         cur = ''
+    #     else:
+    #         cur = cur + letter
+
+    # print(temp)
+    # tournament_names = []
+    # is_print = False
+    # for num, i in enumerate(temp):
+    #     if is_print:
+    #         tournament_names.append(i.split('<')[0])
+    #         is_print = False
+    #     if 'bet-match__picture-text' in i:
+    #         is_print = True
+    #         continue
+
+    # print(len(tournament_names))
+
+    # soup = BeautifulSoup(data, 'html.parser')
+    # parse_html = soup.contents
+    # print(parse_html)
+
+    print('Finishing...')
+
+
+def parse(data):
+
+    return data
+
+
+def get_html(address) -> str:
+    try:
+        settings = webdriver.ChromeOptions()
+        settings.add_argument(f'user-data-dir={abspath("../cache")}')
+        browser = webdriver.Chrome('../resource/lib/chrome_driver.exe', options=settings)
+        browser.set_window_size(1920, 1080)
+        browser.get(address)
+        sleep(10)
+        html = str(browser.page_source)
+        browser.close()
+        return html
+    except WebDriverException:
+        traceback.print_exc(limit=1, file=sys.stdout)
+
+
+def get_test_html(path) -> str:
+    try:
+        with open(path, 'r', encoding='utf-8') as file:
+            return str(file.read())
+    except FileNotFoundError:
+        traceback.print_exc(limit=1, file=sys.stdout)
